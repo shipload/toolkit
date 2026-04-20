8c14717cd10de5b58c94b73294f95d553c4bbe10
aaroncox <aaron.cox@greymass.com>
2026-04-19 22:22:17 -0700

    feat: font sourcing pipeline and /fonts subpath export

---
+++ src/fonts/load-bun.ts
@@ -0,0 +1,16 @@
+import { readFile } from 'node:fs/promises'
+import { dirname, join } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { FONT_MANIFEST, type FontKey } from './index.ts'
+
+const HERE = dirname(fileURLToPath(import.meta.url))
+
+export async function loadFontData(): Promise<Record<FontKey, Uint8Array>> {
+  const entries = await Promise.all(
+    (Object.keys(FONT_MANIFEST) as FontKey[]).map(async (key) => {
+      const buf = await readFile(join(HERE, FONT_MANIFEST[key].fileName))
+      return [key, new Uint8Array(buf)] as const
+    }),
+  )
+  return Object.fromEntries(entries) as Record<FontKey, Uint8Array>
+}
